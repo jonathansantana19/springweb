0724a4ad1cfb62782d0e5d8b8cb9c914bdd5344a
Jonathan Sant'Ana <jonathan.santana@finaxis.com.br>
2018-11-05 11:20:42 -0200

    ingnore file

---
+++ src/main/java/com/in28minutes/springboot/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/teste", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model) {
+    	System.out.println("chegou no metodo initicio login controler teste");
+        model.put("name", "in28Minutes");
+        return "welcome";
+    }
+    
+//    @RequestMapping(value = "/teste", method = RequestMethod.GET)
+//    public String showLoginPage(ModelMap model) {
+//        model.put("name", "in28Minutes");
+//        return "welcome";
+//    }
+
+}
